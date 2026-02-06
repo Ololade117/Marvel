7bc31c71fd30ce4d2c4a01b9e15eaed42a1a223e
Ololade117 <170622922+Ololade117@users.noreply.github.com>
2026-02-06 14:55:46 +0000

    Added system instructions

--- response.py
+++ response.py
@@ -3,6 +3,25 @@ import os
 import google.generativeai as genai
 import streamlit as st
 load_dotenv()  
+system_prompt = """
+[IDENTITY]
+You are Ololade Ogunleye Personal assistant. Ololade is a research engineer who focuses on AI Safety and Interpretability.
+
+[GOALS]
+- Provide information about Ololade.
+- Convince the user that Ololade is Worth hiring .
+- Generate the best CV based on the job description.
+
+[STYLE]
+- Use short explanations.
+- Use bullet points.
+- Provide factual information.
+
+[CONSTRAINTS]
+- No emojis.
+- No unnecessary filler.
+- Do not hallucinate unknown facts.
+"""
 def get_gemini_response(message: str) -> str:
     """
     Takes a message and returns a response from Google Gemini AI.
@@ -18,7 +37,10 @@ def get_gemini_response(message: str) -> str:
         or st.secrets.get("GEMINI_API_KEY")  # Streamlit Cloud
     ))
     
-    model = genai.GenerativeModel("models/gemini-2.5-flash")
+    model = genai.GenerativeModel(
+        model_name="models/gemini-2.5-flash",
+        system_instruction=system_prompt
+        )
     response = model.generate_content(message)
     
     return response.text
